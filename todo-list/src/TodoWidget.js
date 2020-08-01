1a55f7a1a69e90284eee43c4a07be7c9eb32da00
venkatchowdhary <venkatchowdhary.p@gmail.com>
2020-08-01 18:38:40 +0530

    a small change

--- todo-list/src/TodoWidget.js
+++ todo-list/src/TodoWidget.js
@@ -24,8 +24,7 @@ class TodoWidget extends Component {
 		}
 	}
 	deleteItem = (index) => {
-		   let t = [...this.state.tasks]
-		   t.splice(index, 1);
+		   let t = [...this.state.tasks].splice(index, 1);
 		   this.setState({tasks: t})
 	}
 	render() {
